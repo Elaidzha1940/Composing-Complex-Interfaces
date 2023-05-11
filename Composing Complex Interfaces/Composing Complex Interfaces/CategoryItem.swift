32ae72c78abab8b9cc5d98d72a5e1a8a7d0ccb94
Elaidzha1940 <eli080293@gmail.com>
2023-05-11 10:23:33 +0700

    added a new Files / CategoryRow + CategoryItem

---
+++ Composing Complex Interfaces/Composing Complex Interfaces/CategoryItem.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: Composing Complex Interfaces
+//  File: CategoryItem.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 11.05.2023
+//
+//  */
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
