d643c37d5ca3b825633d1582dc2ca38fb3b76e94
Elaidzha1940 <eli080293@gmail.com>
2023-05-11 10:18:28 +0700

    added a new Folder / Model + and new File / ModelData

--- Composing Complex Interfaces/Composing Complex Interfaces/CategoryHome.swift
+++ Composing Complex Interfaces/Composing Complex Interfaces/CategoryHome.swift
@@ -10,12 +10,16 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+
     var body: some View {
-        
         NavigationView {
-            Text("CategoryHome")
-                .navigationTitle("Featured")
-            
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         }
     }
 }
@@ -23,5 +27,6 @@ struct CategoryHome: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(ModelData())
     }
 }
