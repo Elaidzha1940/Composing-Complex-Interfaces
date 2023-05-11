46b1391db4e57535e5b9d55a56c7128186b11c08
Elaidzha1940 <eli080293@gmail.com>
2023-05-11 10:04:29 +0700

    Composing Complex Interfaces

---
+++ Composing Complex Interfaces/Composing Complex Interfaces/Composing_Complex_InterfacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Composing_Complex_InterfacesApp.swift
+//  Composing Complex Interfaces
+//
+//  Created by Elaidzha Shchukin on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Composing_Complex_InterfacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
